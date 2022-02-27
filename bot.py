5c86d2ab343eb112565e11ff33f32c3294303186
mn297 <12944441+mn297@users.noreply.github.com>
2022-02-27 13:29:57 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import json
 from json import JSONDecodeError
 import dateutil
+from discord import HTTPException
 from discord.ext import commands
-# from dotenv import load_dotenv
+import discord
+from dotenv import load_dotenv
 import os
 import random
 from pathlib import Path
@@ -10,9 +12,9 @@ import datetime
 from json import JSONEncoder
 import dateutil.parser
 
-# load_dotenv()
-# TOKEN = os.getenv('DISCORD_TOKEN2')
-TOKEN = os.environ['TOKEN']
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+# TOKEN = os.environ['TOKEN']
 global bot
 bot = commands.Bot(command_prefix='$')
 
@@ -24,6 +26,8 @@ global textDatabase
 global timeDatabase
 global replyOnly
 replyOnly = False
+global current_impersonate
+current_impersonate = 0
 
 # data reload
 fle = Path('data.json')
@@ -60,35 +64,69 @@ with open('time.json') as json_file_time:
         print("decode_time")
         timeDatabase = dict()
         print(timeDatabase)
+# initialize possibleSayings
+if len(textDatabase) != 0:
+    first_key_channel = list(textDatabase.keys())[0]
+    possibleSayings_1 = textDatabase[first_key_channel]
+    first_key_user = list(possibleSayings_1.keys())[0]
+    possibleSayings = possibleSayings_1[first_key_user]
+else:
+    possibleSayings.append('nothing to say, plz impersonate')
+
+async def reload_possibleSayings(userId, channelId):
+    global possibleSayings
+    if str(channelId) in textDatabase:
+        if str(userId) in textDatabase[str(channelId)]:
+            # temporary possibleSayings from offline json
+            possibleSayings = textDatabase[str(channelId)][str(userId)]
+async def append_possibleSayings(userId, channelId):
+    global possibleSayings
+    if str(channelId) in textDatabase:
+        if str(userId) in textDatabase[str(channelId)]:
+            # temporary possibleSayings from offline json
+            possibleSayings += textDatabase[str(channelId)][str(userId)]
 
 
+async def history_helper(ctx, user, userId, channel, channelId, currentMemberCopying, textDatabase, timeDatabase):
+    global possibleSayings
+    global current_impersonate
+    # QUICK CHANGE first, load offline json, TODO quick get name
+    currentMemberCopying = ctx.message.mentions[0].display_name
+    await ctx.guild.me.edit(nick=currentMemberCopying)
+    if current_impersonate != userId:
+        await reload_possibleSayings(userId, channelId)
 
-async def history_helper(ctx, user, userId, channel, channelId, possibleSayings, currentMemberCopying, textDatabase, timeDatabase):
     # fetch messages after if date exist
     h = []
     if str(channelId) in timeDatabase:
         if str(userId) in timeDatabase[str(channelId)]:
             h = await channel.history(limit=9999, after=timeDatabase[str(channelId)][str(userId)]).flatten()
-    else:
+        else:
+            h = await channel.history(limit=9999).flatten()
+    elif str(channelId) not in timeDatabase:
         h = await channel.history(limit=9999).flatten()
+
     # check if user has been called before
     filteredMessages = []
     filteredMessages = [message for message in h if message.author.id == userId]
+
     # check if channel has ever been scraped
     if str(channelId) in timeDatabase:
         if str(userId) in timeDatabase[str(channelId)]:
             print("current time database", timeDatabase[str(channelId)][str(userId)])
             if filteredMessages is not None:
-                filteredMessages = [message for message in filteredMessages if timeDatabase[str(channelId)][str(userId)] < message.created_at]
+                filteredMessages = [message for message in filteredMessages if
+                                    timeDatabase[str(channelId)][str(userId)] < message.created_at]
     else:
         timeDatabase[str(channelId)] = dict()
-   # check if there are new messages
+
+    # check if there are new messages
     if len(filteredMessages) == 0:
         return
     # update time
     timeDatabase[str(channelId)][str(userId)] = filteredMessages[0].created_at
     # to rename bot
-    currentMemberCopying = filteredMessages[0].author.name
+
     possibleSayings_temp = [message.content for message in filteredMessages]
     print(str(channelId) in textDatabase)
     # Update texts to database
@@ -107,11 +145,18 @@ async def history_helper(ctx, user, userId, channel, channelId, possibleSayings,
         textDatabase[str(channelId)][str(userId)] = []
         for text in reversed(possibleSayings_temp):
             textDatabase[str(channelId)][str(userId)].append(text)
-    # new possibleSayings after scrape
-    possibleSayings = textDatabase[str(channelId)][str(userId)]
+
+    # UPDATE possibleSayings
+    if current_impersonate != userId:
+        possibleSayings = textDatabase[str(channelId)][str(userId)]
+    else:
+        await append_possibleSayings(userId, channelId)
+    current_impersonate = userId
+
     # Write to offline database
     with open('data.json', 'w') as fp:
         json.dump(textDatabase, fp, indent=4)
+
     # Write to offline time
     # subclass JSONEncoder
     class DateTimeEncoder(JSONEncoder):
@@ -119,6 +164,7 @@ async def history_helper(ctx, user, userId, channel, channelId, possibleSayings,
         def default(self, obj):
             if isinstance(obj, (datetime.date, datetime.datetime)):
                 return obj.isoformat()
+
     print("Printing to check how it will look like")
     print(DateTimeEncoder().encode(timeDatabase))
     print("Encode DateTime Object into JSON using custom JSONEncoder")
@@ -132,54 +178,113 @@ async def history_helper(ctx, user, userId, channel, channelId, possibleSayings,
 
 @bot.command(name='impersonate')
 async def history(ctx, user):
-    await ctx.send("Obtaining intel. Sit tight.")
     global possibleSayings
     global currentMemberCopying
     global textDatabase
     global timeDatabase
-    # Identify the message
+
+
+    # metadata
     userId = int(user[3:-1])
     channelId = ctx.channel.id
-    await history_helper(ctx, user, userId, ctx.channel, channelId, possibleSayings, currentMemberCopying, textDatabase, timeDatabase)
+
+
+    await ctx.send("Obtaining intel. Sit tight.")
+
+    # Identify the message
+    await history_helper(ctx, user, userId, ctx.channel, channelId, currentMemberCopying, textDatabase, timeDatabase)
+
 
 @bot.command(name='impersonate2')
 async def history(ctx, user):
-    await ctx.send("Obtaining an extreme amount intel. Sit tight.")
     global possibleSayings
     global currentMemberCopying
     global textDatabase
     global timeDatabase
-    # Identify the message
+
+    # Metadata
     userId = int(user[3:-1])
     channel_list = [channel for channel in ctx.guild.channels if isinstance(channel, discord.TextChannel)]
+
+    await ctx.send("Obtaining an extreme amount of intel. Sit tight.")
+
     # scrape all channels
     for channel in channel_list:
-        await history_helper(ctx, user, userId, channel, channel.id, possibleSayings, currentMemberCopying, textDatabase, timeDatabase)
+        await history_helper(ctx, user, userId, channel, channel.id, currentMemberCopying, textDatabase, timeDatabase)
     await ctx.send(f"Done. Now I am 100% {user}")
+
+
+@bot.event
+async def on_ready():
+    # default possibleSayings
+    global possibleSayings
+
+    # reset name to The Impostor
+    for guild in bot.guilds:
+        await guild.me.edit(nick=bot.user.display_name) # or guild.me.display_name
+
+    # reload_possibleSayings(userId, channelId)
+    # get first key in both dict
+    # await bot.process_commands()
+    print("I'm online!")
+
+
+@bot.command(name='ramble')
+async def talk(ctx):
+    try:
+        for i in range(0, 10):
+            await ctx.send(f"DEBUG {i}" + random.choice(possibleSayings))
+    except IndexError:
+        await ctx.send("nothing to say yet, plz impersonate")
+
+
+@bot.command(name='shout')
+async def talk(ctx):
+    try:
+        for i in range(0, 10):
+            await ctx.send(f"DEBUG {i} " + random.choice(possibleSayings).upper())
+    except IndexError:
+        await ctx.send("nothing to say yet, plz impersonate")
+
+
+@bot.command(name='debug')
+async def talk(ctx):
+    try:
+        for i in range(0, 25):
+            try:
+                await ctx.send(possibleSayings[i])
+            except HTTPException:
+                continue
+    except IndexError:
+        await ctx.send("OUT OF THINGS")
+
 
 @bot.command(name='talk')
 async def talk(ctx):
-    await ctx.send(random.choice(possibleSayings))
+    try:
+        await ctx.send("DEBUG " + random.choice(possibleSayings))
+    except IndexError:
+        await ctx.send("nothing to say yet, plz impersonate")
 
 
-@bot.command(name='replyOnly')
+@bot.command(name='replyonly')
 async def replyonly(ctx):
     global replyOnly
     replyOnly = True
-    await ctx.send("Tag me when you need me. Or reply to something I said.")
+    await ctx.send("Impostor turned to reply only")
 
 
 @bot.command(name='on')
 async def on(ctx):
     global replyOnly
     replyOnly = False
-    await ctx.send("The Impostor is now listening.")
+    await ctx.send("Impostor turned to on")
 
 
-@bot.event
-async def on_ready():
-    print("I'm online!")
-    # await bot.process_commands()
+@bot.command(name='info')
+async def info(ctx):
+    await ctx.send(
+        f'List of all commands: \n $impersonate @person - Impersonates the tagged person\n $talk - Forces the Impostor to say something\n $replyonly - Sets the Impostor to only speak when tagged or replied to\n $on - Sets the Impostor to speak when tagged, when replied to, or just randomly')
 
 
 @bot.event
@@ -224,6 +329,8 @@ def getSaying(message, possibleSayings, rgen):
 async def on_message_delete(message):
     await message.channel.send(message.author.name + " just deleted: " + message.content)
     await bot.process_commands(message)
+
+
 
 
 bot.run(TOKEN)
